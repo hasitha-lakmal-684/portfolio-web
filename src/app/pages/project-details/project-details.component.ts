b22a7e2031d397594533b231de1a0b8e56fb8cb5
Hasitha Lakmal <hasitha.lakmal.official@gmail.com>
2025-12-22 14:52:57 +0530

    feat: add project detail page with route-based data loading

--- src/app/pages/project-details/project-details.component.ts
+++ src/app/pages/project-details/project-details.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {Project} from '../../shared/models/project.model';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {ProjectsService} from '../../core/services/projects.service';
+import {NgForOf, NgIf} from '@angular/common';
+import {MatAnchor} from '@angular/material/button';
+import {MatCard, MatCardActions, MatCardContent, MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'app-project-details',
-  imports: [],
+  imports: [
+    NgIf,
+    MatAnchor,
+    RouterLink,
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    NgForOf,
+    MatCardActions
+  ],
   templateUrl: './project-details.component.html',
   styleUrl: './project-details.component.scss'
 })
 export class ProjectDetailsComponent {
+
+  project?: Project;
+
+  constructor(private route: ActivatedRoute, private projectsService: ProjectsService) {
+    const projectId = this.route.snapshot.paramMap.get('id');
+    if (projectId) {
+      this.project = this.projectsService.getById(projectId);
+    }
+  }
 
 }
